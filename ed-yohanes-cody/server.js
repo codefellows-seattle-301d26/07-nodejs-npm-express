8f2b1fcb9eadcbcad89e0e3218019324d17c6d6f
Ed Abrahamsen <esabrahamsen@yahoo.com>
2017-12-13 11:49:41 -0800

    add 404 handler

--- ed-yohanes-cody/server.js
+++ ed-yohanes-cody/server.js
@@ -3,31 +3,26 @@
 
 const express = require('express');
 const app = express();
-
 app.use(express.static('./public'));
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 8007;
-
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.send('Record posted to server!!');
 });
-  
 
-app.get('/new' , (req, res) => { // GET new article '/public/new/'
+app.get('/new' , (req, res) => {
   res.status(200).sendfile('public/new.html');
 });
 
-// app.post('/articles', bodyParser, function(request, response) {
-// // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
-//     console.log(request.body);
-//     response.send('Record posted to server!!');
-//   })
+app.use((req, res) => {
+  res.status(404).send('Sorry can\'t find that page!');
+});
 
 app.get('/bye', (req, res) => {
-  res.json('I am a blob of json')
+  res.json('I am a blob of json');
 });
 // app.post()
 // app.put()
